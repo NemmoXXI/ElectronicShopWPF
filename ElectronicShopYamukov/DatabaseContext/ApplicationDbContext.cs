b88c0f7fc85be6f51daee12db7ee26bfae0d15ea
NemmoXXI <danilyamukov3@gmail.com>
2024-12-14 18:14:55 +0300

    first commit

---
+++ ElectronicShopYamukov/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElectronicShopYamukov.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElectronicShopYamukov.DatabaseContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserEntity>(usersConfigration =>
+            {
+                usersConfigration.HasKey(p => p.Id);
+            });
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;user=root;password=123456789;database=ElectronicShop;",
+                new MySqlServerVersion(new Version(8, 0, 40)));
+        }
+    }
+}
---
+++ ElectronicShopYamukov/Entities/UserEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicShopYamukov.Entities
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public UserEntity() { }
+
+        public UserEntity(
+            string email,
+            string login,
+            string password)
+        {
+            Email = email;
+            Login = login;
+            Password = password;
+        }
+    }
+}
